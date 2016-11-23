7e925ed2a909288fd29b59fdd27a62f29b876dca
陈欣 <95323362@qq.com>
2016-11-23 18:14:21 +0800

    codeForCancelBtn

--- SAMS0/SAMS0/byCourseNameSearchViewController.swift
+++ SAMS0/SAMS0/byCourseNameSearchViewController.swift
@@ -16,6 +16,11 @@ class byCourseNameSearchViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- SAMS0/SAMS0/bySnoSearchViewController.swift
+++ SAMS0/SAMS0/bySnoSearchViewController.swift
@@ -21,6 +21,9 @@ class bySnoSearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancelBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
