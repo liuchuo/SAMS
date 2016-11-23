88a1af3230100bc033d4eeec22c8d5da0b6f4ac3
陈欣 <95323362@qq.com>
2016-11-23 16:15:07 +0800

    添加launchimage

---
+++ SAMS0/SAMS0/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SAMS0
+//
+//  Created by ChenXin on 2016/11/23.
+//  Copyright © 2016年 ChenXin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //launchscreen停留2秒
+        Thread.sleep(forTimeInterval: 2.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
